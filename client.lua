86d1848f55d437c76c89d063ad8f8c89d5cd1caf
Martinator9001 <90502234+Martinator9001@users.noreply.github.com>
2025-08-21 13:53:14 +0300

    Update client.lua
    Signed-off-by: Martinator9001 <90502234+Martinator9001@users.noreply.github.com>

--- client.lua
+++ client.lua
@@ -681,7 +681,7 @@ local function useSlot(slot, noAnim)
 			elseif data.allowArmed then
 				useItem(data)
             else
-                return lib.notify({ id = 'cannot_perform', type = 'error', description = locale('cannot_perform') })
+                return lib.notify({ id = 'weapon_hand_forbidden', type = 'error', description = locale('weapon_hand_forbidden') })
 			end
 		elseif not data.ammo and not data.component then
 			useItem(data)
@@ -1915,3 +1915,4 @@ lib.callback.register('ox_inventory:getVehicleData', function(netid)
 		return GetEntityModel(entity), GetVehicleClass(entity)
 	end
 end)
+
